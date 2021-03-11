57d1be437cd86e01c6d1ec8976684fa7fa9abd06
Str1XHyper <tijnvanveghel@gmail.com>
2021-03-11 16:19:46 +0100

    Fixed $axios

--- animetracker/src/main.js
+++ animetracker/src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
+
+axios.defaults.baseURL = window.location.hostname === "localhost" ? "http://localhost:3000" : ""
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+
 
 new Vue({
   router,
